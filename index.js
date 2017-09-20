3e7dffb3fba578a222fc8b83e174a5836092fbc6
Orrett Bailey <orrettlb@gmail.com>
2017-09-20 09:58:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
